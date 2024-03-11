bb1fae0c1282cb1a74c9d91e0569ce47d6eff029
Ozan Müyesseroğlu <ozanmuyes@gmail.com>
2024-03-11 23:34:14 +0300

    Make JS slimmer

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 const argon2 = require('.');
 
 // 12345
@@ -5,15 +7,9 @@ const argon2 = require('.');
 
 const plaintext = "12345";
 
-console.log("Hashing...");
 const hash = argon2.hash(plaintext);
-console.log(hash);
 
-let isVerified;
+assert(argon2.verify(plaintext, hash));
+assert(!argon2.verify(plaintext + 'x', hash));
 
-console.log("Verifying...");
-isVerified = argon2.verify(plaintext, hash)
-console.log(isVerified ? "Verified (should match)" : "!!! NOT VERIFIED !!! (SHOULD MATCH)");
-
-isVerified = argon2.verify(plaintext + 'x', hash)
-console.log(!isVerified ? "Verified (should not match)" : "!!! NOT VERIFIED !!! (SHOULD NOT MATCH)");
+console.log('OK');
